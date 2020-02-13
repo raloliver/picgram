9d2042a6c1de2dc2037635de6bbca64dc7b7e624
LIMA OLIVEIRA Israel <ilimaoliveira@europe.corp.altran.com>
2020-02-13 16:40:35 +0000

    Init search component

--- src/app/photos/photo-list/photo-list.module.ts
+++ src/app/photos/photo-list/photo-list.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PhotoListComponent } from './photo-list.component';
 import { PhotoModule } from '../photo/photo.module';
+import { SearchComponent } from './search/search.component';
 
 
 
 @NgModule({
-  declarations: [PhotoListComponent],
+  declarations: [PhotoListComponent, SearchComponent],
   imports: [
     CommonModule,
     PhotoModule
